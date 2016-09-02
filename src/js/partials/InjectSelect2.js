670f438b88704cc136ac0387ed075f3d131d0446
Robert Spier <robert.spier@gmail.com>
2016-09-02 15:14:57 +0200

    We now have a working select2 environment.

--- src/js/partials/InjectSelect2.js
+++ src/js/partials/InjectSelect2.js
@@ -1,12 +1,12 @@
 /* global $ */
 
-import jquery from 'jQuery';
-import "select2";
+import 'select2';
 
 (function() {
-
 	function activateSelect2() {
-		console.log($.select());
+		$('select').select2({
+			minimumResultsForSearch: Infinity
+		});
 	}
 
 	$(document).ready(() => {
