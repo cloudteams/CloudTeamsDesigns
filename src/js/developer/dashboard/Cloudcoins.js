96d76f086d61be0eeddaf91bacd9428675757e00
Robert Spier <robert.spier@gmail.com>
2016-09-19 17:01:34 +0200

    push

--- src/js/developer/dashboard/Cloudcoins.js
+++ src/js/developer/dashboard/Cloudcoins.js
@@ -1,7 +1,51 @@
 /* global $ */
 
 (function() {
-	$(document).ready(() => {
 
+	function confirmMessage() {
+		const confirmButton = $('.confirm-button');
+		const contentSection = $('.entire-section');
+		const confirmSection = $('.confirm-wrapper');
+		const sendingSection = $('.sending-wrapper');
+		const modalBody = $('.modal-body');
+
+		function fadeDelay() {
+			setTimeout(() => {
+				sendingSection.fadeOut(() => {
+					confirmSection.fadeIn();
+				});
+			}, 2000);
+
+			setTimeout(() => {
+				confirmSection.fadeOut(() => {
+					modalBody.css('height', 'auto');
+					contentSection.fadeIn();
+				});
+			}, 4000);
+		}
+
+		confirmButton.click(e => {
+			e.preventDefault();
+
+			const height = modalBody.outerHeight();
+			modalBody.css('height', `${height}`);
+
+			contentSection.fadeOut(() => {
+				sendingSection.fadeIn();
+			});
+
+			fadeDelay();
+		});
+	}
+
+	function initClicks() {
+		$('main a').click(e => {
+			e.preventDefault();
+		});
+	}
+
+	$(document).ready(() => {
+		initClicks();
+		confirmMessage();
 	});
 })();
--- src/views/developer/dashboard/cloudcoins/transfer-cloudcoins.php
+++ src/views/developer/dashboard/cloudcoins/transfer-cloudcoins.php
@@ -9,7 +9,7 @@
 				<div class="vertical-align">
 					<div class="middle">
 						<i class="icon icon-cloudcoins"></i>
-						<h2 class="header-medium secondary">Add new document</h2>
+						<h2 class="header-medium secondary">Transfer CloudCoins</h2>
 					</div>
 					<div class="close" data-dismiss="modal">
 						<i class="icon icon-close"></i>
@@ -18,114 +18,63 @@
 			</header>
 
 			<div class="modal-body">
-				<div class="row">
-					<div class="col-md-6">
-						<fieldset class="form-group fieldset-upload">
-							<div class="input-container">
-								<label class="file-upload-image" for="file-upload">
-									<div class="vertical-align">
-										<div class="middle">
-											<div class="add-image-button">
-												<i class="icon icon-plus"></i>
-											</div>
-											<p class="meta big">Add picture</p>
-										</div>
-									</div>
-								</label>
-								<input type="file" name="file-upload" id="file-upload"/>
-							</div>
-						</fieldset>
 
-					</div>
-
-					<div class="col-md-6">
-						
-						<fieldset class="form-group required">
-							<label for="add-reward-title" class="header-medium secondary control-label">Reward title</label>
-
-							<div class="input-container">
-								<input type="number" id="add-reward-title"  placeholder="Reward title"></textarea>
-							</div>
-						</fieldset>
-
-						<fieldset>
-							<label for="description-document" class="header-medium secondary">Description</label>
-
-							<div class="input-container">
-								<textarea id="description-document" rows="5" placeholder="Project description"></textarea>
-							</div>
-						</fieldset>
-
-					</div>
-
-				</div>
 				<div class="row">
 
 					<div class="col-md-6">
 						<fieldset class="form-group required">
-							<label for="add-reward-amount" class="header-medium secondary control-label">Amount of rewards</label>
-
-							<div class="input-container">
-								<input type="number" id="add-reward-amount"  placeholder="Amount of rewards"></textarea>
-							</div>
-						</fieldset>
-					</div>
-
-					<div class="col-md-6">
-						<fieldset class="form-group required">
-							<label for="add-reward-price" class="header-medium secondary control-label">Rewards price</label>
-
-							<div class="input-container">
-								<input type="number" id="add-reward-price"  placeholder="Rewards price"></textarea>
-							</div>
-						</fieldset>
-					</div>
-
-				</div>
-				<div class="row">
-
-					<div class="col-md-12">
-						<fieldset class="form-group required">
-							<label for="add-reward-amount" class="header-medium secondary control-label">What type of reward is it?</label>
-
+							<label for="add-from-amount" class="header-medium secondary control-label">From</label>
 							<div class="select-dropdown">
 								<i class="icon icon-arrow-down"></i>
-								<select name="category-project" id="category-project" onchange="" size="1">
-									<option value="" disabled="" selected="">Select type</option>
-									<option value="0">Link</option>
-									<option value="1">Coupon</option>
-									<option value="2">Other</option>
+								<select name="from-project" id="from-project" onchange="" size="1">
+									<option value="" disabled="" selected="">Select project</option>
+									<option value="0">Project 1</option>
+									<option value="1">Project 2</option>
+									<option value="2">Project 3</option>
 								</select>
 							</div>
 						</fieldset>
 					</div>
 
-					<div class="col-md-12">
+					<div class="col-md-6">
 						<fieldset class="form-group required">
-							<label for="add-reward-link" class="header-medium secondary control-label">Paste your link here</label>
-
-							<div class="input-container">
-								<input type="text" id="add-reward-link"  placeholder="Link to reward"></input>
-							</div>
-						</fieldset>
-					</div>
-
-					<div class="col-md-12">
-						<fieldset class="form-group fieldset-checkbox required">
-							<div class="input-container">
-								<label for="number-the-questions"><input type="checkbox" name="question-numbers" value='1' id="number-the-questions"><span></span>I agree to the <a href="#">terms and conditions</a> of the CloudTeam Trust Policy</label>
+							<label for="add-reward-price" class="header-medium secondary control-label">To</label>
+							<div class="select-dropdown">
+								<i class="icon icon-arrow-down"></i>
+								<select name="to-project" id="to-project" onchange="" size="1">
+									<option value="" disabled="" selected="">Select type</option>
+									<option value="0">Project 1</option>
+									<option value="1">Project 2</option>
+									<option value="2">Project 3</option>
+								</select>
 							</div>
 						</fieldset>
 					</div>
 
 				</div>
+
+				<div class="row">
+
+					<div class="col-md-6">
+						<p class="meta">How many CloudCoins would you like to transfer?</p>
+					</div>
+
+					<div class="col-md-6">
+						<fieldset class="form-group required">
+							<div class="input-container">
+								<input type="number" id="add-reward-price"  placeholder="Write a number"></textarea>
+							</div>
+						</fieldset>
+					</div>
+
+				</div>				
 				<hr>
 
 				<div class="row">
 					<div class="col-md-12">
 						<fieldset class="form-group form-submit">
 							<a href="#nowhere" data-dismiss="modal" class="btn-transparent" alt="cancel">Cancel</a>
-							<a href="#nowhere" data-dismiss="modal" class="btn confirm-button" alt="create">Create</a>
+							<a href="#nowhere" class="btn confirm-button" alt="create">Create</a>
 						</fieldset>
 					</div>
 				</div>
