4143098aa23660d59d385f919b0fde3be11830bb
Robert Spier <robert.spier@gmail.com>
2016-09-23 14:47:13 +0200

    Applied same fix for Documents.js

--- src/js/developer/dashboard/Documents.js
+++ src/js/developer/dashboard/Documents.js
@@ -16,13 +16,39 @@
 				.next();
 
 		sibling.fadeOut();
-		selectAllOptionsOpen();
+		selectOptionOpen(_el);
 	}
 
 	function selectAllOptionsOpen() {
 		const edit = $('a[data-target="extend-row"]');
 
 		edit.click(e => {
+			e.preventDefault();
+
+			const row = $(e.target)
+				.closest('.row')
+				.next();
+
+			row.fadeIn();
+
+			const arrow = $(e.target)
+				.closest('.row')
+				.find('i.icon-arrow-down');
+
+			arrow
+				.css({
+					'transform': 'scaleY(-1)'
+				});
+
+			arrow.parent().off().click(ev => {
+				ev.preventDefault();
+				closeOptions(ev.target);
+			});
+		});
+	}
+
+	function selectOptionOpen(el) {
+		$(el).closest('a').click(e => {
 			e.preventDefault();
 
 			const row = $(e.target)
