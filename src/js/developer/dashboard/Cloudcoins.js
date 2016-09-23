c2fddfcc779cec3fc0bfa425c6358a10e494716a
Robert Spier <robert.spier@gmail.com>
2016-09-23 14:38:04 +0200

    Revamped javascript to be scalable, as well as restructured cloudcoin dropdowns

--- src/js/developer/dashboard/Cloudcoins.js
+++ src/js/developer/dashboard/Cloudcoins.js
@@ -27,8 +27,6 @@
 		confirmButton.click(e => {
 			e.preventDefault();
 
-			console.log('hello')
-
 			const height = modalBody.outerHeight();
 			modalBody.css('height', `${height}`);
 
@@ -40,6 +38,78 @@
 		});
 	}
 
+	function closeOptions(el) {
+		const _el = $(el);
+
+		_el
+			.off()
+			.css({
+				'transform': 'scaleY(1)'
+			});
+
+		const sibling =
+			_el
+				.closest('.row')
+				.next();
+
+		sibling.fadeOut();
+		selectOptionOpen(_el);
+	}
+
+	function selectAllOptionsOpen() {
+		const edit = $('a[data-target="extend-row"]');
+
+		edit.click(e => {
+			e.preventDefault();
+
+			const row = $(e.target)
+				.closest('.row')
+				.next();
+
+			row.fadeIn();
+
+			const arrow = $(e.target)
+				.closest('.row')
+				.find('i.icon-arrow-down');
+
+			arrow
+				.css({
+					'transform': 'scaleY(-1)'
+				});
+
+			arrow.parent().off().click(ev => {
+				ev.preventDefault();
+				closeOptions(ev.target);
+			});
+		});
+	}
+
+	function selectOptionOpen(el) {
+		$(el).closest('a').click(e => {
+			e.preventDefault();
+
+			const row = $(e.target)
+				.closest('.row')
+				.next();
+
+			row.fadeIn();
+
+			const arrow = $(e.target)
+				.closest('.row')
+				.find('i.icon-arrow-down');
+
+			arrow
+				.css({
+					'transform': 'scaleY(-1)'
+				});
+
+			arrow.parent().off().click(ev => {
+				ev.preventDefault();
+				closeOptions(ev.target);
+			});
+		});
+	}
+
 	function initClicks() {
 		$('main a').click(e => {
 			e.preventDefault();
@@ -48,6 +118,7 @@
 
 	$(document).ready(() => {
 		initClicks();
+		selectAllOptionsOpen();
 		confirmMessage();
 	});
 })();
--- src/views/developer/dashboard/cloudcoins/content.php
+++ src/views/developer/dashboard/cloudcoins/content.php
@@ -16,11 +16,11 @@
 			</article>
 
 			<article class="cloudcoins-row-item col-lg-2">
-				<p><i class="icon icon-cloudcoins"></i>4600</p>
+				<p class="cloudcoins-align-right"><i class="icon icon-cloudcoins"></i>6000</p>
 			</article>
 
 			<article class="cloudcoins-row-item col-lg-1">
-				<div class="icon-container">
+				<div class="icon-container cloudcoins-align-right">
 					<a data-target="extend-row" href=""><i class="icon icon-arrow-down"></i></a>
 				</div>
 			</article>
@@ -28,7 +28,73 @@
 		</div>
 
 		<div style="display:none;" class="row cloudcoins-content-row">
-			<p>hi</p>
+
+			<div class="cloudcoins-row-header">
+				<article class="cloudcoins-row-item col-lg-5">
+					<p>Campaign name</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p>Status</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">Cloudcoins given</p>
+				</article>
+			</div>
+
+			<div class="cloudcoins-row-content">
+				<article class="cloudcoins-row-item col-lg-5">
+					<p><a href="#">Campaign 1#</a></p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p>Draft</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">600</p>
+				</article>
+			</div>
+
+			<div class="cloudcoins-row-content">
+				<article class="cloudcoins-row-item col-lg-5">
+					<p><a href="#">Campaign 1#</a></p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p>Draft</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">300</p>
+				</article>
+			</div>
+
+			<div class="cloudcoins-row-content">
+				<article class="cloudcoins-row-item col-lg-5">
+					<p><a href="#">Campaign 1#</a></p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p>Draft</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">190</p>
+				</article>
+			</div>
+
+			<div class="cloudcoins-row-total">
+				<article class="cloudcoins-row-item col-lg-8">
+					<p>Total</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">1130</p>
+				</article>
+			</div>
+
 		</div>
 
 		<div class="row cloudcoins-row">
@@ -38,14 +104,172 @@
 			</article>
 
 			<article class="cloudcoins-row-item col-lg-2">
-				<p><i class="icon icon-cloudcoins"></i>4600</p>
+				<p class="cloudcoins-align-right"><i class="icon icon-cloudcoins"></i>3500</p>
 			</article>
 
 			<article class="cloudcoins-row-item col-lg-1">
-				<div class="icon-container">
+				<div class="icon-container cloudcoins-align-right">
 					<a data-target="extend-row" href=""><i class="icon icon-arrow-down"></i></a>
 				</div>
 			</article>
+
+		</div>
+
+		<div style="display:none;" class="row cloudcoins-content-row">
+
+			<div class="cloudcoins-row-header">
+				<article class="cloudcoins-row-item col-lg-5">
+					<p>Campaign name</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p>Status</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">Cloudcoins given</p>
+				</article>
+			</div>
+
+			<div class="cloudcoins-row-content">
+				<article class="cloudcoins-row-item col-lg-5">
+					<p><a href="#">Campaign 1#</a></p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p>Draft</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">600</p>
+				</article>
+			</div>
+
+			<div class="cloudcoins-row-content">
+				<article class="cloudcoins-row-item col-lg-5">
+					<p><a href="#">Campaign 1#</a></p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p>Draft</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">300</p>
+				</article>
+			</div>
+
+			<div class="cloudcoins-row-content">
+				<article class="cloudcoins-row-item col-lg-5">
+					<p><a href="#">Campaign 1#</a></p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p>Draft</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">190</p>
+				</article>
+			</div>
+
+			<div class="cloudcoins-row-total">
+				<article class="cloudcoins-row-item col-lg-8">
+					<p>Total</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">1130</p>
+				</article>
+			</div>
+
+		</div>
+
+		<div class="row cloudcoins-row">
+
+			<article class="cloudcoins-row-item col-lg-9">
+				<p>Project 3#</p>
+			</article>
+
+			<article class="cloudcoins-row-item col-lg-2">
+				<p class="cloudcoins-align-right"><i class="icon icon-cloudcoins"></i>4600</p>
+			</article>
+
+			<article class="cloudcoins-row-item col-lg-1">
+				<div class="icon-container cloudcoins-align-right">
+					<a data-target="extend-row" href=""><i class="icon icon-arrow-down"></i></a>
+				</div>
+			</article>
+
+		</div>
+
+		<div style="display:none;" class="row cloudcoins-content-row">
+
+			<div class="cloudcoins-row-header">
+				<article class="cloudcoins-row-item col-lg-5">
+					<p>Campaign name</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p>Status</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">Cloudcoins given</p>
+				</article>
+			</div>
+
+			<div class="cloudcoins-row-content">
+				<article class="cloudcoins-row-item col-lg-5">
+					<p><a href="#">Campaign 1#</a></p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p>Draft</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">600</p>
+				</article>
+			</div>
+
+			<div class="cloudcoins-row-content">
+				<article class="cloudcoins-row-item col-lg-5">
+					<p><a href="#">Campaign 1#</a></p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p>Draft</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">300</p>
+				</article>
+			</div>
+
+			<div class="cloudcoins-row-content">
+				<article class="cloudcoins-row-item col-lg-5">
+					<p><a href="#">Campaign 1#</a></p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p>Draft</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">190</p>
+				</article>
+			</div>
+
+			<div class="cloudcoins-row-total">
+				<article class="cloudcoins-row-item col-lg-8">
+					<p>Total</p>
+				</article>
+
+				<article class="cloudcoins-row-item col-lg-3">
+					<p class="cloudcoins-align-right">1130</p>
+				</article>
+			</div>
 
 		</div>
 
