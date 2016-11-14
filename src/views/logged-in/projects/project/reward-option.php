869170352b437278cd04b39bbe2464ab45b53a94
Robert Spier <robert.spier@gmail.com>
2016-11-14 14:08:51 +0100

    Force URLs to be white and bold from now on in act/not

--- src/views/logged-in/projects/project/reward-option.php
+++ src/views/logged-in/projects/project/reward-option.php
@@ -1,37 +1,19 @@
 <article class="reward public-reward block-line-half col-lg-4 col-md-6 clear-3">
 	<div class="background">
-		<header class="article-header">
-			<h3 class="header-medium secondary">Give feedback on our homepage and participate in...</h3>
-			<p class="highlight bold">Project title</p>
-
-			<div class="tooltip-container">
-				<a href="#nowhere" class="open-tooltip-button"><i class="icon icon-more-options"></i></a>
-				<div class="custom-tooltip">
-					<ul>
-						<li>
-							<a href="">Delete reward</a>
-						</li>
-						<li><a href="">Lorem ipsum</a></li>
-						<li><a href="">Lorem ipsum</a></li>
-					</ul>
-				</div>
-			</div>
+		<header class="article-header reward-header">
+			<h3 class="header-medium secondary">Reward 1#</h3>
+			<img src="[[../../../../img/placeholders/project_thumbnail.svg]]" alt="Team user logo">
 		</header>
 
-		<div class="content dates">
-			<i class="icon icon-start"></i>
-			<p><time datetime="2016-02-30">30/02/2016</time></p>
-
-			<i class="icon icon-close2"></i>
-			<p><time datetime="2016-05-17">17/05/2016</time></p>
+		<div class="content">
+			<p class="highlight bold">This is a reward description</p>
 		</div>
 
 		<div class="content data">
 			<table>
 				<tr class="row">
 					<td class="col-xs-5">
-						<i class="icon icon-add-member"></i>
-						<p>Participants</p>
+						<p>Remaining</p>
 					</td>
 					<td class="col-xs-7">
 						<p class="bold">22</p>
@@ -40,8 +22,7 @@
 
 				<tr class="row">
 					<td class="col-xs-5">
-						<i class="icon icon-time"></i>
-						<p>Days left</p>
+						<p>Reward price</p>
 					</td>
 					<td class="col-xs-7">
 						<p class="bold">54</p>
