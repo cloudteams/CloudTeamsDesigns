826a966c7e99e5c64852a8c56c5da9e2f78173a7
Robert Spier <robert.spier@gmail.com>
2016-11-16 14:01:09 +0100

    Padding & font size on questionnaire + choose popup

--- src/views/developer/dashboard/projects/project/campaigns/campaign/modal/choose-questionnaire.php
+++ src/views/developer/dashboard/projects/project/campaigns/campaign/modal/choose-questionnaire.php
@@ -20,21 +20,55 @@
 			<div class="modal-body">
 				<div class="row choose-row-header">
 					<div class="col-md-7">
-						Name
+						<strong>Name</strong>
 					</div>
 					<div class="col-md-5">
-						Questions
+						<strong>Questions</strong>
 					</div>
 				</div>
+
 				<div class="row choose-row">
 					<div class="col-md-7">
-						Name
+						A random questionnaire
 					</div>
 					<div class="col-md-3">
-						Questions
+						5 questions
 					</div>
 					<div class="col-md-2">
-						Questions
+						<a href="#">Delete</a>
+					</div>
+				</div>
+
+				<div class="row choose-row">
+					<div class="col-md-7">
+						A random questionnaire
+					</div>
+					<div class="col-md-3">
+						3 questions
+					</div>
+					<div class="col-md-2">
+						<a href="#">Delete</a>
+					</div>
+				</div>
+
+				<div class="row choose-row">
+					<div class="col-md-7">
+						A random questionnaire
+					</div>
+					<div class="col-md-3">
+						8 questions
+					</div>
+					<div class="col-md-2">
+						<a href="#">Delete</a>
+					</div>
+				</div>
+
+				<div class="row submit-row">
+					<div class="col-md-12">
+						<fieldset class="form-group form-submit">
+							<a href="#nowhere" data-dismiss="modal" class="btn-transparent" alt="cancel">Cancel</a>
+							<a href="#nowhere" data-dismiss="modal" class="btn confirm-button" alt="save">Choose</a>
+						</fieldset>
 					</div>
 				</div>
 			</div>
