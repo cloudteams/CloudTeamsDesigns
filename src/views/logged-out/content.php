c42bc54710cd7b002f62957179c4bf6e95ffbf6e
Robert Spier <robert.spier@gmail.com>
2016-12-12 16:40:29 +0100

    Updated main page

--- src/views/logged-out/content.php
+++ src/views/logged-out/content.php
@@ -20,8 +20,9 @@
 				<div class="col-md-8 preview"><img src="[[../../img/user-screenshot.png]]" alt="User screenshot"></div>
 				<div class="col-md-4 context">
 					<div class="explanation">
-						<h2>hi</h2>
-						<p>test</p>
+						<span><i class="icon icon-project"></i></span>
+						<h2 class="header-medium">Participate in projects to gain rewards</h2>
+						<p>You have your own dashboard where you can see all the projects you are following or invited for.</p>
 					</div>
 				</div>
 			</div>
@@ -31,15 +32,43 @@
 	<section id="subpage-2" class="subpage">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-4 context"><p>This is some text as explanation</p></div>
+				<div class="col-md-4 context">
+					<div class="explanation">
+						<span><i class="icon icon-project"></i></span>
+						<h2 class="header-medium">Participate in projects to gain rewards</h2>
+						<p>You have your own dashboard where you can see all the projects you are following or invited for.</p>
+					</div>
+				</div>			
 				<div class="col-md-8 preview"><img src="[[../../img/user-screenshot.png]]" alt="User screenshot"></div>
 			</div>
 		</div>
 	</section>
 
+	<section class="slideshow-navigation">
+		<nav>
+			<ul>
+				<li><i class="icon icon-arrow-left"></i></li>
+				<li><span></span></li>
+				<li><span class="active"></span></li>
+				<li><i class="icon icon-arrow-right"></i></li>
+			</ul>
+		</nav>
+	</section>
+
 </section>
 
 <section class="public-projects-logged-in-content">
+
+	<div class="call-to-action">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<p>Are you a developer? <a href="/views/logged-out/#latest-projects">SUBMIT A PROJECT</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
 	<div class="container">
 		<header id="latest-projects">
 			<h2 class="header-large public">Latest projects</h2>
@@ -82,6 +111,10 @@
 				</div>
 			</div>
 		</div>
+	</div>
+
+	<div class="back-to-top">
+		<a href="#"><i class="icon icon-arrow-up"></i></a>
 	</div>
 
 </section>
