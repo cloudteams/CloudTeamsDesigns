90820e4565ac6aed495d080c1daf68afa9900c59
martijnvhummel <martijnvhummel@hotmail.com>
2017-02-08 14:59:54 +0100

    Removed 'transfer cloudcoins button'

--- src/views/developer/dashboard/cloudcoins/content.php
+++ src/views/developer/dashboard/cloudcoins/content.php
@@ -4,7 +4,7 @@
 		<div class="row">
 			<article class="col-lg-12 cloudcoins cloudcoins-add-button col-md-12">
 
-				<h2 class="header-medium secondary">You currently have 3 projects <a href="#" class="btn add-new-cloudcoins" alt="add cloudcoins" data-toggle="modal" data-target=".transfer-cloudcoins-to-project">Transfer CloudCoins</a></h2>
+				<h2 class="header-medium secondary">You currently have 3 projects</h2>
 
 			</article>
 		</div>
